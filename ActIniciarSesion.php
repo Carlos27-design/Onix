ecbadcbf475e4568ede636b62e1cdb458378d0bd
Desector1234 <coronadoc899@gmail.com>
2020-04-06 16:44:08 -0400

    Accion Iniciar Sesion

---
+++ ActIniciarSesion.php
@@ -0,0 +1,33 @@
+<?php
+include_once 'DB/Usuario.php';
+include_once 'DB/UsuarioDB.php';
+
+session_start();
+
+if(isset($_POST['txtRut']) && isset($_POST['txtContrasena']))
+{
+    $rut = $_POST['txtRut'];
+    $contrasena = $_POST['txtContrasena'];
+
+    if($rut && $contrasena != null)
+    {
+        $usuario = new Usuario();
+        $UsuarioDB = new UsuarioDB();
+        $usu = $UsuarioDB->login($rut, $contrasena);
+        
+        if($usu != null)
+        {
+            $_SESSION['usu'] = $usu;
+            header("Location: index.php");
+        }else
+        {
+            $_SESSION['message'] = '<div class="alert alert-danger">Usuario no encontrado</div>';
+            header("Location: index.php");
+        }
+    }
+    else
+    {
+        header("Location: index.php");
+    }
+    
+}
