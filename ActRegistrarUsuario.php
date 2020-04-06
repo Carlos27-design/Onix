fe1bc648dabb5a78d213d9e56c2579885fbf7659
Katalina25 <katalina.guerrero02@inacapmail.cl>
2020-04-06 17:27:02 -0400

    registrar usuario, nav, footer

---
+++ ActRegistrarUsuario.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+
+include_once 'DB/Usuario.php';
+include_once 'DB/UsuarioDB.php';
+
+
+$ok = true;
+$message = null;
+
+$usuario = new Usuario();
+$usuarioDB = new UsuarioDB();
+
+$txtRut = null;
+$txtNombre = null;
+$txtApellido = null;
+$txtContrasena = null;
+$txtCorreo = null;
+$txtNroTelefonico = null;
+$tipoUsuarioId = null;
+
+if (
+    isset($_POST['txtRut']) && isset($_POST['txtNombre']) && isset($_POST['txtApellido'])
+    && isset($_POST['txtContrasena']) && isset($_POST['txtCorreo'])
+    && isset($_POST['txtNroTelefonico']) && isset($_POST['tipoUsuarioId'])
+) {
+    $txtRut =  $_POST['txtRut'];
+    $txtNombre = $_POST['txtNombre'];
+    $txtApellido =  $_POST['txtApellido'];
+    $txtContrasena =  $_POST['txtContrasena'];;
+    $txtCorreo =  $_POST['txtCorreo'];;
+    $txtNroTelefonico =  $_POST['txtNroTelefonico'];;
+    $tipoUsuarioId = $_POST['tipoUsuarioId'];
+
+
+    if (($txtRut && $txtNombre && $txtApellido && $txtContrasena &&
+        $txtCorreo && $txtNroTelefonico && $tipoUsuarioId) != "") {
+        $usuario->id = 0;
+        $usuario->rut = $txtRut;
+        $usuario->nombre = $txtNombre;
+        $usuario->apellido = $txtApellido;
+        $usuario->contrasena = $txtContrasena;
+        $usuario->correo = $txtCorreo;
+        $usuario->nroTelefonico = $txtNroTelefonico;
+        $usuario->tipoUsuario_id = $tipoUsuarioId;
+
+        $ok = $usuarioDB->crear($usuario);
+    } else {
+        $message = "Debe ingresar todos los datos";
+        $ok = false;
+    }
+}
+
+
+if ($ok) {
+    $_SESSION['message'] = '<div class="alert alert-success">
+  Usuario Registrado Correctamente
+  <a href="#">Bienvenido</a> 
+  </div>';
+} else {
+    $_SESSION['message'] = '<div class="alert alert-danger">
+  ' . $message . '</div>';
+}
+
+header("Location: index.php");
---
+++ footer.php
@@ -0,0 +1,48 @@
+<div class="footer-copyright-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+                <div class="footer-copyright wow fadeIn">
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    <p>Copyright &copy;<script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
+                <div class="footer-social-bookmark text-right wow fadeIn">
+                    <ul class="social-bookmark">
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-vimeo"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!--FOOER AREA END-->
+
+
+
+<!--====== SCRIPTS JS ======-->
+<script src="js/vendor/jquery-1.12.4.min.js"></script>
+<script src="js/vendor/bootstrap.min.js"></script>
+
+<!--====== PLUGINS JS ======-->
+<script src="js/vendor/jquery.easing.1.3.js"></script>
+<script src="js/vendor/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/vendor/jquery.appear.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/stellar.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/stellarnav.min.js"></script>
+<script src="js/contact-form.js"></script>
+<script src="js/jquery.sticky.js"></script>
+
+<!--===== ACTIVE JS=====-->
+<script src="js/main.js"></script>
