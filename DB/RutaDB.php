440f3365394819bacb31607081847b3f2cb9b666
Katalina25 <katalina.guerrero02@inacapmail.cl>
2020-04-06 14:15:32 -0400

    restauracion

--- DB/RutaDB.php
+++ DB/RutaDB.php
@@ -5,7 +5,6 @@ include_once 'Ruta.php';
 
 class RutaDB extends Conexion
 {
-<<<<<<< HEAD
 
     function crear($ruta)
     {
@@ -20,18 +19,6 @@ class RutaDB extends Conexion
         $stmt = $this->miConexion->prepare($sql) or trigger_error($this->miConexion->error . "[$sql]");
         $stmt->bind_param("isssss", $ruta->id, $ruta->direccionInicio, $ruta->direccionFinal, $ruta->distancia, $ruta->fechaInicio, $ruta->fechaFin);
         $ok = $stmt->execute();
-=======
-    function crear($ruta)
-    {
-        $this->conectar();
-
-        $sql = "INSERT INTO ruta (id, direccionInicio, direccionFinal, distancia, fechaInicio, fechaFin) 
-                VALUES(?,?,?,?,?,?)";
-        
-        $stmt = $this->miConexion->prepare($sql);
-        $stmt->bind_param("issddss", $ruta->id, $ruta->direccionInicio, $ruta->direccionFinal, $ruta->distancia, $ruta->fechaInicio, $ruta->fechaFinal);
-        $ok=$stmt->execute();
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
         $this->desconectar();
 
         return $ok;
@@ -39,7 +26,6 @@ class RutaDB extends Conexion
 
     function listar()
     {
-<<<<<<< HEAD
 
         $arreglo = array();
         $this->conectar();
@@ -54,32 +40,12 @@ class RutaDB extends Conexion
 
             $arreglo[] = $estado;
         }
-=======
-        $arreglo = array();
-        $this->conectar();
-
-        $sql="SELECT * from ruta";
-        $resultados = $this->miConexion->query($sql);
-        while($fila = $resultado->fetch_assoc())
-        {
-            $ruta = new Ruta();
-            $ruta->id = $fila['id'];
-            $ruta->direccionInicio = $fila['direccionInicio'];
-            $ruta->direcionFinal = $fila['direccionFinal'];
-            $ruta->distancia = $fila['distancia'];
-            $ruta->fechaInicio = $fila['fechaInicio'];
-            $ruta->fechaFin = $fila['fechaFin'];
-
-            $arreglo[] = $ruta;
-        } 
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
 
         $this->desconectar();
 
         return $arreglo;
     }
 
-<<<<<<< HEAD
     function buscar($id)
     {
 
@@ -99,36 +65,11 @@ class RutaDB extends Conexion
             $devolver = $estado;
         }
 
-=======
-    function Buscar($id)
-    {
-        $devolver = null;
-        $this->conectar();
-
-        $sql = "SELECT * FROM ruta where id = ?";
-        $stmt = $this->miConexion->prepare(sql);
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $resultados = $stmt->get_result();
-        while($fila=$resultados->fetch_assoc())
-        {
-            $ruta = new Ruta();
-            $ruta->id = $fila['id'];
-            $ruta->direccionInicio = $fila['direccionInicio'];
-            $ruta->direcionFinal = $fila['direccionFinal'];
-            $ruta->distancia = $fila['distancia'];
-            $ruta->fechaInicio = $fila['fechaInicio'];
-            $ruta->fechaFin = $fila['fechaFin'];
-            $devolver = $ruta;
-
-        }
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
         $this->desconectar();
 
         return $devolver;
     }
 
-<<<<<<< HEAD
     function editar($estado)
     {
         $this->conectar();
@@ -138,48 +79,19 @@ class RutaDB extends Conexion
         $stmt->bind_param("si", $estado->nombre, $estado->id);
         $ok = $stmt->execute();
         $this->desconectar();
-=======
-    function Editar($ruta)
-    {
-        $this->conectar();
-        $sql="UPDATE ruta set 
-        direccionInicio=?, direccionFinal=?, fechaInicio=?, fechaFin=?";
-        $stmt = $this->miConexion->prepare($sql);
-        $stmt->bind_param("ssss", $ruta->direccionInicio, $ruta->direccionFinal, $ruta->fechaInicio, $ruta->fechaFin);
-        $ok=$stmt->execute();
-        $this->desconectar();
-        
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
 
         return $ok;
     }
 
-<<<<<<< HEAD
 
     function eliminar($id)
     {
         $this->conectar();
         $sql = "DELETE from estado where id = ?";
-=======
-    function Eliminar($id)
-    {
-        $this->conectar();
-        $sql = "DELETE from ruta where id = ?";
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
         $stmt = $this->miConexion->prepare($sql);
         $stmt->bind_param("i", $id);
         $ok = $stmt->execute();
         $this->desconectar();
-<<<<<<< HEAD
         return $ok;
     }
 }
-=======
-
-        return $ok;
-    }
-
-    
-
-}
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
--- DB/UsuarioDB.php
+++ DB/UsuarioDB.php
@@ -5,7 +5,6 @@ include_once 'Usuario.php';
 
 class UsuarioDB extends Conexion
 {
-<<<<<<< HEAD
 
     function crear($usuario)
     {
@@ -18,18 +17,6 @@ class UsuarioDB extends Conexion
         $stmt = $this->miConexion->prepare($sql);
         $stmt->bind_param("issssssi", $usuario->id, $usuario->rut, $usuario->nombre, $usuario->apellido, $usuario->contrasena, $usuario->correo, $usuario->nroTelefonico, $usuario->tipoUsuario_id);
         $ok = $stmt->execute();
-=======
-    function crear($usuario)
-    {
-        $this->conectar();
-
-        $sql = "INSERT INTO usuario (id, rut, nombre, apellido, contrasena, correo, nroTelefonico, tipoUsuario_id) 
-                VALUES(?,?,?,?,?,?,?,?)";
-        
-        $stmt = $this->miConexion->prepare($sql);
-        $stmt->bind_param("issssssi", $usuario->id, $usuario->rut, $usuario->nombre, $usuario->apellido, $usuario->contrasena, $usuario->correo, $usuario->nroTelefonico, $usuario->tipoUsuario_id);
-        $ok=$stmt->execute();
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
         $this->desconectar();
 
         return $ok;
@@ -37,7 +24,6 @@ class UsuarioDB extends Conexion
 
     function listar()
     {
-<<<<<<< HEAD
 
         $arreglo = array();
         $this->conectar();
@@ -45,21 +31,11 @@ class UsuarioDB extends Conexion
         $sql = "SELECT * from usuario";
         $resultados = $this->miConexion->query($sql);
         while ($fila = $resultados->fetch_assoc()) {
-=======
-        $arreglo = array();
-        $this->conectar();
-
-        $sql="SELECT * from usuario";
-        $resultados = $this->miConexion->query($sql);
-        while($fila = $resultado->fetch_assoc())
-        {
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
             $usuario = new Usuario();
             $usuario->id = $fila['id'];
             $usuario->rut = $fila['rut'];
             $usuario->nombre = $fila['nombre'];
             $usuario->apellido = $fila['apellido'];
-<<<<<<< HEAD
             $usuario->contrasena = $fila['contrasena'];
             $usuario->correo = $fila['correo'];
             $usuario->nroTelefonico = $fila['nroTelefonico'];
@@ -67,20 +43,12 @@ class UsuarioDB extends Conexion
 
             $arreglo[] = $usuario;
         }
-=======
-            $usuario->nroTelefonico = $fila['nroTelefonico'];
-            $usuario->tipoUsuario_id = $fila['tipoUsuario_id'];
-            
-            $arreglo[] = $usuario;
-        } 
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
 
         $this->desconectar();
 
         return $arreglo;
     }
 
-<<<<<<< HEAD
     function buscar($id)
     {
 
@@ -93,26 +61,11 @@ class UsuarioDB extends Conexion
         $stmt->execute();
         $resultados = $stmt->get_result();
         while ($fila = $resultados->fetch_assoc()) {
-=======
-    function Buscar($id)
-    {
-        $devolver = null;
-        $this->conectar();
-
-        $sql = "SELECT * FROM usuario where id = ?";
-        $stmt = $this->miConexion->prepare(sql);
-        $stmt->bind_param("i", $id);
-        $stmt->execute();
-        $resultados = $stmt->get_result();
-        while($fila=$resultados->fetch_assoc())
-        {
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
             $usuario = new Usuario();
             $usuario->id = $fila['id'];
             $usuario->rut = $fila['rut'];
             $usuario->nombre = $fila['nombre'];
             $usuario->apellido = $fila['apellido'];
-<<<<<<< HEAD
             $usuario->contrasena = $fila['contrasena'];
             $usuario->correo = $fila['correo'];
             $usuario->nroTelefonico = $fila['nroTelefonico'];
@@ -121,20 +74,11 @@ class UsuarioDB extends Conexion
             $devolver = $usuario;
         }
 
-=======
-            $usuario->nroTelefonico = $fila['nroTelefonico'];
-            $usuario->tipoUsuario_id = $fila['tipoUsuario_id'];
-            
-            $arreglo[] = $usuario;
-
-        }
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
         $this->desconectar();
 
         return $devolver;
     }
 
-<<<<<<< HEAD
     function editar($usuario)
     {
         $this->conectar();
@@ -144,28 +88,12 @@ class UsuarioDB extends Conexion
         $stmt->bind_param("ssssssii", $usuario->rut, $usuario->nombre, $usuario->apellido, $usuario->contrasena, $usuario->correo, $usuario->nroTelefonico, $usuario->tipoUsuario_id, $usuario->id);
         $ok = $stmt->execute();
         $this->desconectar();
-=======
-    function Editar($ruta)
-    {
-        $this->conectar();
-        $sql="UPDATE usuario set 
-        rut=?, nombre=?, apellido=?, contrasena=?, correo=?, nroTelefonico=?";
-        $stmt = $this->miConexion->prepare($sql);
-        $stmt->bind_param("ssssss", $usuario->rut, $usuario->nombre, $usuario->apellido, $usuario->contrasena, $usuario->correo, $usuario->nroTelefonico );
-        $ok=$stmt->execute();
-        $this->desconectar();
-        
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
 
         return $ok;
     }
 
-<<<<<<< HEAD
 
     function eliminar($id)
-=======
-    function Eliminar($id)
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
     {
         $this->conectar();
         $sql = "DELETE from usuario where id = ?";
@@ -173,16 +101,6 @@ class UsuarioDB extends Conexion
         $stmt->bind_param("i", $id);
         $ok = $stmt->execute();
         $this->desconectar();
-<<<<<<< HEAD
         return $ok;
     }
 }
-=======
-
-        return $ok;
-    }
-
-    
-
-}
->>>>>>> b7cbbeaf7a72c3a2427eaf8cfb997d7688fe03a0
