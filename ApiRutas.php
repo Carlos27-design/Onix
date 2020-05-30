aac46c6f0db2ce340e40df5ee8efffecaf852058
Katalina25 <katalina.guerrero02@inacapmail.cl>
2020-05-30 14:56:10 -0400

    ruta listas

---
+++ ApiRutas.php
@@ -0,0 +1,17 @@
+<?php
+$hostname_localhost = "localhost";
+$database_localhost = "id";
+$username_localhost = "root";
+$password_localhost = "12345";
+
+$json = array();
+
+$conexion = mysqli_connect($hostname_localhost, $username_localhost, $password_localhost, $database_localhost);
+$consulta = "select * from rutas";
+$resultado = mysqli_query($conexion, $consulta);
+
+while ($row = mysqli_fetch_row($resultado)) {
+    $json[] = $row[0];
+}
+mysqli_close($conexion);
+echo json_encode($json);
