d566976b389cfff3bd72185892fcb7fc52ab7d1c
Katalina25 <katalina.guerrero02@inacapmail.cl>
2020-06-02 18:01:04 -0400

    ruta

--- mostrarRuta.php
+++ mostrarRuta.php
@@ -1,17 +1,15 @@
 <?php
-$hostname_localhost = "localhost";
-$database_localhost = "onix";
-$username_localhost = "root";
-$password_localhost = "password";
+session_start();
+
+include_once 'DB/Ruta.php';
+include_once 'DB/RutaDB.php';
+
+$rutaDB = new RutaDB();
+$ruta = new Ruta();
+
 
 $datos = array();
+$datos[]  = $rutaDB->listar();
 
-$conexion = mysqli_connect($hostname_localhost, $username_localhost, $password_localhost, $database_localhost);
-$consulta = "select * from ruta";
-$resultado = mysqli_query($conexion, $consulta);
 
-while ($row = mysqli_fetch_row($resultado)) {
-    $datos[] = $row;
-}
 echo json_encode($datos);
-mysqli_close($conexion);
